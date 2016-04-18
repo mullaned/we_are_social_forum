a663cda9d6f93309bbacf5f88289b2eed87e7de0
Richard Dalton <richard@devjoy.com>
2016-04-18 11:22:34 +0100

    Initial Commit

---
+++ magazines/migrations/0004_auto_20160417_2249.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('magazines', '0003_auto_20160417_2128'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='purchase',
+            name='subscription_end',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+    ]
